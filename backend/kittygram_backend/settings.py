24eedfddd6e2231da2cc2911686da04f6e69efbe
dalukyanov <hanatsu.tori@gmail.com>
2026-01-06 20:36:17 +0300

    Fix: DJANGO_SECRET_KEY в переменной окружения 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,6 +5,8 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# По комментарию от ревьювера ключ добавлен в качестве переменной окружения
+# в секреты, либо генерится на ходу
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') or get_random_secret_key()
 
 DEBUG = True
